d2fb789beb82212b72c4d341a6d1866bc30cccdf
wudi <wudi1@udesk.cn>
2021-12-20 22:31:43 +0800

    init

---
+++ src/main/java/com/xianyv/pangu/PanguApplication.java
@@ -0,0 +1,13 @@
+package com.xianyv.pangu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanguApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PanguApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xianyv/pangu/PanguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xianyv.pangu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanguApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
